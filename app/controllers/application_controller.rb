2ff81bf3a7229ab012a59cf305d8d8bb64f85283
Hadja Sow <maimouna.sow917@gmail.com>
2017-05-06 16:05:48 +0000

    Update.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   def hello
-     render html: "Whatever message you want!"
+     render html: Random.rand(1...100)
   end
 end
