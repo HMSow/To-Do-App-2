60b120746040c08b7392fc9cc443aacb75303b4c
Hadja Sow <maimouna.sow917@gmail.com>
2017-05-07 19:01:53 +0000

    Homework

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'list/1' => 'list#first'
-  get 'list/2' => 'list#second'
+  get 'list/:id' => 'list#show'
   root'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
